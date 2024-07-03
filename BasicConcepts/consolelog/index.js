7fc2d133c255b8ba0d9919c58c53b432e69c3e8a
Dogolaa <lucasdogo01@gmail.com>
2024-07-03 19:32:04 -0300

    BasicConcepts_1

---
+++ BasicConcepts/consolelog/index.js
@@ -0,0 +1,17 @@
+var teste = 'amor';
+var numero = 13;
+
+console.log(' ' + teste + ' ' + numero);
+
+console.log('Meu nome é "Lucas Dogo".Estou aprendendo JavaScript as ' + 10 + ' da manhã.' )
+
+/* Comentario */
+
+// Comentario
+
+/**
+ *  Um Titulo
+ * 
+ * Lorem ipsum
+ */
+
