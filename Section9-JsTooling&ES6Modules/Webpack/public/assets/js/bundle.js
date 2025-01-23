817b4ac1007d28274dc37438a640cf0463791be8
Dogolaa <lucasdogo01@gmail.com>
2025-01-22 21:19:55 -0300

    Model-Webpack

--- Section9-JsTooling&ES6Modules/Webpack/public/assets/js/bundle.js
+++
@@ -1,8 +0,0 @@
-/******/ (() => { // webpackBootstrap
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-console.log('Hello World');
-/******/ })()
-;
-//# sourceMappingURL=bundle.js.map
