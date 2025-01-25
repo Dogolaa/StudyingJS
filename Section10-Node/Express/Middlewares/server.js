300f41927dad2a7febf366a15e09d15c913f1878
Dogolaa <lucasdogo01@gmail.com>
2025-01-24 21:50:49 -0300

    middlewares

---
+++ Section10-Node/Express/Middlewares/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const path = require('path');
+const { middlewareGlobal } = require('./src/middlewares/middleware');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(middlewareGlobal);
+app.use(routes);
+
+app.get('/testes/:idUsuarios?/:parametro?', (req, res) => {
+  console.log(req.params);
+  console.log(req.query);
+  //res.send(req.params);
+  res.send(req.query.nome);
+});
+
+app.listen(3000, () => {
+  console.log('Acessar http://localhost:3000 ');
+  console.log('Servidor executando na porta 3000');
+});
