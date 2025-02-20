2ef71164ed022a712b12340ea36b7ebfb78b6d76
Lucas Dogo <lucasdogo01@gmail.com>
2025-02-20 11:24:27 -0300

    LINK

--- Section15-ReactRouterDom_v6/ReactRouteDomV6/src/main.tsx
+++ Section15-ReactRouterDom_v6/ReactRouteDomV6/src/main.tsx
@@ -5,10 +5,12 @@ import { Home } from "./components/Home";
 import { About } from "./components/About";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Menu } from "./components/Menu";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
